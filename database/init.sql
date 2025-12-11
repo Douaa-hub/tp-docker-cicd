4fe45ac4dd0ba4c9b10cbbda1fddd25f23fcf1ea
NetAcad DEVASC <changeme@example.com>
2025-12-11 00:58:39 +0000

    Add init.sql + fix DB + clean CI

--- database/init.sql
+++ database/init.sql
@@ -1,9 +1,7 @@
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  email VARCHAR(100) UNIQUE NOT NULL
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100)
 );
 
-INSERT INTO users (name, email) VALUES
-('Alice', 'alice@example.com'),
-('Bob', 'bob@example.com');
+INSERT INTO users (name) VALUES ('Alice'), ('Bob'), ('Charlie');
+
